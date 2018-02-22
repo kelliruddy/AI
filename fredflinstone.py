7b7be753ea644ca2117793f3ea2c626385cef5c4
kelliruddy <kelli.ruddy@yahoo.com>
2018-02-22 13:41:28 -0700

    Fred Flinstone

---
+++ fredflinstone.py
@@ -0,0 +1,14 @@
+# def loadBoard(fileName):
+#     f = open('fileName','w');
+#     newBoard = [];
+#
+#     return newBoard;
+#
+# def printBoard(newBoard):
+#     print loadedBoard;
+#
+# def possibleMoves((x,y),loadedBoard):
+#     return possible;
+#
+# def legalMoves(possible,)
+print "Hello"
